acd718d2ea330d9a9eb3df43df0fbb29c4516808
rangkas <rangkas.karangrejo@gmail.com>
2022-08-06 18:34:52 +0700

    first commit

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -35,7 +35,7 @@ class Database extends Config
         'hostname' => 'localhost',
         'username' => 'root',
         'password' => '',
-        'database' => 'kendaraan',
+        'database' => 'sipp',
         'DBDriver' => 'MySQLi',
         'DBPrefix' => '',
         'pConnect' => false,
